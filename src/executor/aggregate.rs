ae12779c594b4f535607c984ef720814fa690223
LemarAb <janbleyer@web.de>
2022-12-27 15:28:25 +0100

    add impl for all selects

--- src/executor/aggregate.rs
+++ src/executor/aggregate.rs
@@ -1,7 +1,14 @@
-use crate::{error::*, ColumnTrait, ConnectionTrait};
+use crate::{
+    error::*, ColumnTrait, ConnectionTrait, EntityTrait, FromQueryResult, Select, SelectModel,
+    SelectTwo, SelectTwoModel, Selector, SelectorTrait,
+};
 use sea_query::SelectStatement;
+
 #[async_trait::async_trait]
-pub trait AggregatorTrait {
+pub trait AggregatorTrait<'db, C>
+where
+    C: ConnectionTrait,
+{
     fn aggregate(self, db: &'db C) -> Aggregator<'db, C>;
 }
 
@@ -13,10 +20,46 @@ where
     pub(crate) db: &'db C,
 }
 
-impl<'db, C> Aggregator<'db, C> {
+impl<'db, C> Aggregator<'db, C>
+where
+    C: ConnectionTrait,
+{
     pub fn count<T>(col: T) -> Result<i64, DbErr>
     where
         T: ColumnTrait,
     {
+    }
+}
+
+impl<'db, C, S> AggregatorTrait<'db, C> for Selector<S>
+where
+    C: ConnectionTrait,
+    S: SelectorTrait + Send + Sync + 'db,
+{
+    fn aggregate(self, db: &'db C) -> Aggregator<'db, C> {
+        todo!()
+    }
+}
+
+impl<'db, C, E, M> AggregatorTrait<'db, C> for Select<E>
+where
+    C: ConnectionTrait,
+    E: EntityTrait<Model = M>,
+{
+    fn aggregate(self, db: &'db C) -> Aggregator<'db, C> {
+        todo!()
+    }
+}
+
+impl<'db, C, M, N, E, F> AggregatorTrait<'db, C> for SelectTwo<E, F>
+where
+    C: ConnectionTrait,
+    E: EntityTrait<Model = M>,
+    F: EntityTrait<Model = N>,
+    M: FromQueryResult + Sized + Send + Sync + 'db,
+    N: FromQueryResult + Sized + Send + Sync + 'db,
+{
+    fn aggregate(self, db: &'db C) -> Aggregator<'db, C> {
+        todo!()
     }
 }
